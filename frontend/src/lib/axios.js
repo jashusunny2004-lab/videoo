328b2f07cbc93262b149225b830f97acbe87e35c
JashwanthYerra2404 <jashwanthyerra2404@gmail.com>
2025-10-19 00:12:32 +0530

    changed axios.js in frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
+const BASE_URL = process.env.VITE_BASE_URL || "http://localhost:5000/api";
+
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: BASE_URL,
     withCredentials: true // send the cookies with the request
 })
